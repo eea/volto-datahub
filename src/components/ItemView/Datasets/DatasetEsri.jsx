3c1adb2f2b38c2fbc3527dc7db89fe3c6e62caef
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-09-08 11:06:28 +0300

    Avoid code duplicates

--- src/components/ItemView/Datasets/DatasetEsri.jsx
+++ src/components/ItemView/Datasets/DatasetEsri.jsx
@@ -5,57 +5,36 @@ import lockSVG from 'remixicon/icons/System/lock-line.svg';
 import { isInternalURL } from '@eeacms/volto-datahub/utils';
 import servicesSVG from '../icons/services.svg';
 
+const ItemLink = ({ item }) => (
+  <List.Item key={item.id}>
+    <List.Content>
+      <div>
+        <SVGIcon name={servicesSVG} size="20px" />
+        <a
+          className="item-link"
+          href={item.url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {item.protocol} {item.name || item.description}
+        </a>
+        {isInternalURL(item.url) && <SVGIcon name={lockSVG} size="20px" />}
+      </div>
+    </List.Content>
+  </List.Item>
+);
+
 export default function DatasetEsri({ dataset }) {
   return (
     (dataset['ESRI:REST'] || dataset['OGC:WMS']) && (
       <>
         <h5>Services:</h5>
         <List divided relaxed>
-          {(dataset['ESRI:REST'] || []).map((item, i) => {
-            return (
-              <List.Item key={i}>
-                <List.Content>
-                  <div>
-                    <SVGIcon name={servicesSVG} size="20px" />
-                    <a
-                      className="item-link"
-                      href={item.url}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {item.protocol} {item.name || item.description}
-                    </a>
-                    {isInternalURL(item.url) && (
-                      <SVGIcon name={lockSVG} size="20px" />
-                    )}
-                  </div>
-                </List.Content>
-              </List.Item>
-            );
-          })}
-
-          {(dataset['OGC:WMS'] || []).map((item, i) => {
-            return (
-              <List.Item key={i}>
-                <List.Content>
-                  <div>
-                    <SVGIcon name={servicesSVG} size="20px" />
-                    <a
-                      className="item-link"
-                      href={item.url}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {item.protocol} {item.name || item.description}
-                    </a>
-                    {isInternalURL(item.url) && (
-                      <SVGIcon name={lockSVG} size="20px" />
-                    )}
-                  </div>
-                </List.Content>
-              </List.Item>
-            );
-          })}
+          {[...(dataset['ESRI:REST'] || []), ...(dataset['OGC:WMS'] || [])].map(
+            (item, i) => (
+              <ItemLink item={item} key={item.id} />
+            ),
+          )}
         </List>
       </>
     )
